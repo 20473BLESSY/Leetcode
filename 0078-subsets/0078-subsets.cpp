0da15516d7bae7fb2ff7f2768da869641ff3bce5
20473BLESSY <92619472+20473BLESSY@users.noreply.github.com>
2023-02-27 23:10:16 +0530

    Time: 6 ms (29.77%), Space: 7.2 MB (56.75%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>>res;
+        int n = nums.size();
+        for(int i=0;i<pow(2,n);i++)
+        {   
+            vector<int>arr;
+            for(int j=0;j<n;j++)
+            {
+                if((i>>j)&1 ==1)
+                {
+                    arr.push_back(nums[j]);
+                }
+            }
+            res.push_back(arr);
+        }
+        return res;    
+        
+    }
+};
