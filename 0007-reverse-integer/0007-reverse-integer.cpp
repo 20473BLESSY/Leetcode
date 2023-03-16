ad6148358f45e4a5aee2cb4d37d006c461b2f635
Blessy Boorla <92619472+20473BLESSY@users.noreply.github.com>
2023-03-16 22:32:58 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (92.97%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long long n = x;
+        x = abs(n);
+         long long rev =0;
+            while(x>0)
+            {
+                rev =(rev*10)+ x%10;
+                x= x/10;
+            }
+        if((rev<= INT_MIN) || (rev>=INT_MAX)){
+            return 0;
+            
+        }
+        else{
+        if(n>0)  return rev;
+        
+        else return rev*-1;
+        }
+        
+        
+    
+    }
+};
