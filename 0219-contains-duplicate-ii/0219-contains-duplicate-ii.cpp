b74882d82ac0bf07cb1942d654028732f27e2648
Blessy Boorla <92619472+20473BLESSY@users.noreply.github.com>
2023-03-27 22:25:51 +0530

    Time: 158 ms (98.55%), Space: 77.2 MB (69.84%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -4,11 +4,12 @@ public:
         unordered_map<int,int>m;
         for(int i=0;i<nums.size();i++)
         {
-            if(m.count(nums[i]))
+            if(m.find(nums[i]) != m.end())
             {
                 if(abs(i-m[nums[i]])<=k)
                 {
                     return true;
+                    break;
                     
                 }
             }
