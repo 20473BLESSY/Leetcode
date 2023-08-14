564cf1c2093e5e2333b1422be6244014e9e26af0
Blessy Boorla <92619472+20473BLESSY@users.noreply.github.com>
2023-08-14 18:34:47 +0530

    Time: 3 ms (84.15%), Space: 9.1 MB (63.39%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -0,0 +1,19 @@
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+       sort(strs.begin(),strs.end());
+        string ans ="";
+        int n = strs.size();
+        
+        for(int i=0;i<min(strs[0].size(),strs[n-1].size());i++)
+        {
+           if((strs[0][i]) != (strs[n-1][i]))
+           {
+               return ans;
+           }
+            ans += strs[0][i];
+        }
+        return ans;
+    }
+};
