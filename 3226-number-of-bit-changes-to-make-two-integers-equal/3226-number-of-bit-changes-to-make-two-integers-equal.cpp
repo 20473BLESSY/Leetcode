081a6ead985805b4ddc4839a5900e0f39fd47f2a
Blessy Boorla <92619472+20473BLESSY@users.noreply.github.com>
2024-07-23 22:56:46 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (34.19%) - LeetHub

---
+++ 3226-number-of-bit-changes-to-make-two-integers-equal/3226-number-of-bit-changes-to-make-two-integers-equal.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minChanges(int n, int k) {
+        int cnt=0;
+            
+            for(int i=0;i<32;i++)
+            {
+                if((!(k&1<<i)) && (n&1<<i))
+                {
+                    cnt++;
+                }
+                
+                if(((k&1<<i)) && (!(n&1<<i))) { return -1;}
+               
+        }
+         return cnt;
+    }
+};
